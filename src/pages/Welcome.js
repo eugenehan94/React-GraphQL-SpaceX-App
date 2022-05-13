7391a62c2580cbb1ff18a670fe3e69070f6d3b6b
eugenehan94 <eugene.han94@gmail.com>
2022-05-13 19:39:57 -0400

    change welcome component with video

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -17,19 +17,35 @@ const Welcome = () => {
         sx={{
           height: "100%",
           flexGrow: 1,
-          p: {sm: 0, md: 3 },
+          p: { sm: 0, md: 3 },
           width: { sm: `calc(100% - ${DRAWER_WIDTH}px)` },
           height: "100vh",
         }}
       >
         <Toolbar />
-          <Box sx={{ height: "100%"}}>
-              <img
-                src={spaceXWallpaper}
-                alt="spaceX"
-                style={{ width: "100%", height: "100%", objectFit: "fill" }}
-              />
-          </Box>
+        <Box sx={{ height: "100%" }}>
+          {/* <img
+            src={spaceXWallpaper}
+            alt="spaceX"
+            style={{ width: "100%", height: "100%", objectFit: "fill" }}
+          /> */}
+          {/* <iframe
+            src="https://www.youtube.com/embed/E7wJTI-1dvQ"
+            frameborder="0"
+            allow="autoplay; encrypted-media"
+            allowfullscreen
+            title="video"
+          /> */}
+          <iframe
+            width="100%"
+            height="100%"
+            src="https://www.youtube.com/embed/Tk338VXcb24"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+        </Box>
       </Box>
     </Box>
   );
